a4ed700e165b68a95e6f7889771199d22870b3c7
ACounterman <56138907+alexcounterman@users.noreply.github.com>
2025-02-24 13:48:04 -0500

    Update App.js
    added navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import Login from './pages/Login';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>Instrument Qualification</Typography>
+          <Button color="inherit" component={Link} to="/">Login</Button>
+          <Button color="inherit" component={Link} to="/home">Home</Button>
+        </Toolbar>
+      </AppBar>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
